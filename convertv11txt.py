9c5196991d8ba119609fb09a28543e067058655d
kolci <kolci@duck.com>
2024-12-10 00:12:56 +0100

    Adjusted the script from csv to another

--- convertv11txt.py
+++ convertv11txt.py
@@ -4,28 +4,38 @@ Convert the CVC-YOLOv3 format to YOLOv11 txt format
 
 import csv
 import os
-import re
 
 # Normalize bounding box
-def NormalizeBbox(bbox,width,height,id):
-    x,y,h,w = [int(coord) for coord in bbox.strip('[]').split(',')]
-    x = x
-    y = y
-    w = w
-    h = h
-    id = 1 # This is an id that need to be changed, since the data don't tell which class, leave it 1
+def NormalizeBbox(x,y,w,h,width,height):
     centerX = (x + w/2) / width
     centerY = (y + h/2) / height
     normWidth = w / width
     normHeight = h / height
-    return f"{id} {centerX:.6f} {centerY:.6f} {normWidth:.6f} {normHeight:.6f}"
+    return centerX, centerY, normWidth, normHeight
 
-# Clear Corrupted Data
-def cleanbboxData(bboxData):
-    cleaned = re.findall(r'\[\s*\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\]',bboxData)
-    return cleaned
+def toNestedList(data):
+    processed = []
+    temp = []
+    count = -1
+    for i in range (len(data)):
+        count += 1
+        if count == 5:
+            processed.append(temp)
+            temp = []
+            count = 0
+        if data[i][0] == '[':
+            temp.append(data[i][1:])
+        elif data[i][0] == '"':
+            temp.append(data[i][2:])
+        elif data[i][-1] == '"':
+            temp.append(data[i][:-2])
+        else:
+            temp.append(data[i])
+        
+    
+    return processed
 
-csvPath = "./inputs/yolov3-training-validate.csv"
+csvPath = "./inputs/labels.csv"
 outputDir = "./output/"
 
 os.makedirs(outputDir,exist_ok=True)
@@ -33,20 +43,26 @@ os.makedirs(outputDir,exist_ok=True)
 with open(csvPath) as f:
     reader = csv.reader(f)
     for row in reader:
-        # Skip if the name of the image is empty
-        if row[0] == '':
+        if not row[0] or not row[0].strip(): # Skip empty rows
             continue
-        imageName = row[0].strip()
-        imageWidth = int(row[2])
-        imageHeight = int(row[3])
-        scale = float(row[4])
-        rawBboxes = ','.join(row[5:])
-
-        bboxes = cleanbboxData(rawBboxes)
-
+        if row[1] != '':
+            continue
+        allInfo = row[0].strip().split(',')
+        imageName = allInfo[0]
+        if imageName == 'Name':
+            continue
+        imageWidth = int(allInfo[2])
+        imageHeight = int(allInfo[3])
+        scale = float(allInfo[4])
+        
+        rawbboxes = list(allInfo[5:])
+        bboxes = toNestedList(rawbboxes)
         txtFile = os.path.splitext(imageName)[0] + '.txt'
 
         with open(os.path.join(outputDir,txtFile),'w') as txt:
-            for i,bbox in enumerate(bboxes):
-                id = i
-                txt.write(NormalizeBbox(bbox,imageWidth,imageHeight,id) + '\n')
+            for bbox in bboxes:
+                x,y,h,w,id = map(int,bbox)
+                cX,cY,nW,nH = NormalizeBbox(x,y,w,h,imageWidth,imageHeight)
+                txt.write(f"{int(id)} {cX:.6f} {cY:.6f} {nW:.6f} {nH:.6f}\n")
+
+        print(f"Done {txtFile}")
